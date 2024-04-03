11153b663a23bf8afaec53bbd5207f04f12de35d
Jaehyeon Park <skystar@skystar.dev>
2024-04-03 22:22:24 +0900

    gomod2nix

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [
+        (import "${fetchTree gomod2nix.locked}/overlay.nix")
+      ];
+    }
+  )
+, buildGoApplication ? pkgs.buildGoApplication
+}:
+
+buildGoApplication {
+  pname = "kubectl-trace";
+  version = "v20231026";
+  pwd = ./.;
+  src = ./.;
+  modules = ./gomod2nix.toml;
+}
